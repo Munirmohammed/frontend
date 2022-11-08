220d70c762a4bc1936527fbf2ad2cd7535f4d7d9
Munir Mohammed <munirmohammed038@gmail.com>
2022-11-08 20:50:45 +0300

    frontend

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { SiMeetup } from 'react-icons/si'
+import { Link } from 'react-router-dom'
+import './Home.scss'
+
+const Home = () => {
+  return (
+    <div className="home">
+      <nav className="container --flex-between">
+        <div className="logo">
+          <SiMeetup size={35} />
+        </div>
+        <ul className="home-links">
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <button className="--btn --btn-primary">
+              <Link to="/login">Login</Link>
+            </button>
+          </li>
+          <li>
+            <button className="--btn --btn-primary">
+              <Link to="/dashboard">Dashboard</Link>
+            </button>
+          </li>
+        </ul>
+      </nav>
+
+      <section className="container hero">
+        <div className="hero-text">
+          <h2>Inventory and Stock Management Solution</h2>
+          <p>
+            Inventory system to control and manage proucts in the warehouse in
+            real timeand integrated to make it easier to develop your business.
+          </p>
+          <div className="hero-buttons">
+            <button className="--btn --btn-secondary">
+              <Link to="/dashboard">Free Trial 1 Month</Link>
+            </button>
+          </div>
+          <div className="--flex-start">
+            <NumberText num="14K" text="Brand Owners" />
+          </div>
+        </div>
+        <div className="hero-image"></div>
+      </section>
+    </div>
+  )
+}
+
+const NumberText = ({ num, text }) => {
+  return (
+    <div className="--mr">
+      <h3 className="--color-white">{num}</h3>
+      <p className="--color-white">{text}</p>
+    </div>
+  )
+}
+
+export default Home
